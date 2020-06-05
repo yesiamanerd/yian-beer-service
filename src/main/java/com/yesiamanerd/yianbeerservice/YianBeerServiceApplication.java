ad7f010a1e88f46ff27a44ee6f69785befa27dfb
yesiamanerd <yesiamanerd@gmail.com>
2020-06-05 14:08:39 -0400

    Initial Project Setup - Additional files. closes #1

---
+++ src/main/java/com/yesiamanerd/yianbeerservice/YianBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yesiamanerd.yianbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YianBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YianBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yesiamanerd/yianbeerservice/YianBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yesiamanerd.yianbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YianBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
